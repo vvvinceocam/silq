ddac8c9c32c1ee889d5b9c03a44c54ca3434996a
vvvince <vvvince@ofcompute.rs>
2023-07-13 16:45:45 +0200

    feat: add basic exception mechanism

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![forbid(unsafe_code)]
 #![cfg_attr(windows, feature(abi_vectorcall))]
 
+use std::collections::HashMap;
+use std::error::Error;
+
 use ext_php_rs::prelude::*;
 use ext_php_rs::types::ZendClassObject;
 use http::request::Builder;
@@ -10,7 +13,6 @@ use hyper::header::HeaderName;
 use hyper::http::response::Parts;
 use hyper::{HeaderMap, Method};
 use once_cell::sync::OnceCell;
-use std::collections::HashMap;
 use tokio::net::TcpStream;
 use tokio::runtime::Runtime;
 
@@ -18,6 +20,28 @@ static RUNTIME: OnceCell<Runtime> = OnceCell::new();
 
 fn get_runtime() -> &'static Runtime {
     RUNTIME.get().expect("Unitizialied Spidroin Runtime")
+}
+
+pub struct SpidroinError {
+    pub description: String,
+}
+
+impl SpidroinError {
+    pub fn new(description: String) -> Self {
+        Self { description }
+    }
+
+    pub fn from<T: Error>(context: &str, error: &T) -> Self {
+        Self {
+            description: format!("{context}: {}", error),
+        }
+    }
+}
+
+impl From<SpidroinError> for PhpException {
+    fn from(value: SpidroinError) -> PhpException {
+        PhpException::default(format!("Spidroin Exception: {}", value.description))
+    }
 }
 
 /// HTTP client
@@ -31,8 +55,10 @@ impl HttpClient {
     }
 
     /// Execute a GET request to the specified URL. Returns a Response object.
-    pub fn get(&self, uri: &str) -> RequestBuilder {
-        let uri = uri.parse::<hyper::Uri>().unwrap();
+    pub fn get(&self, uri: &str) -> PhpResult<RequestBuilder> {
+        let uri = uri
+            .parse::<hyper::Uri>()
+            .map_err(|err| SpidroinError::from("Unable to parse URI", &err))?;
 
         // Get the host and the port
         let host = uri.host().expect("uri has no host");
@@ -41,15 +67,17 @@ impl HttpClient {
         let address = format!("{}:{}", host, port);
 
         // The authority of our URL will be the hostname with port
-        let authority = uri.authority().unwrap().clone();
+        let authority = uri
+            .authority()
+            .ok_or_else(|| SpidroinError::new("Unable to extract authority".to_string()))?
+            .clone();
 
         // Create an HTTP request with an empty body and a HOST header
         let builder = hyper::Request::builder()
             .method(Method::GET)
             .uri(uri)
             .header(hyper::header::HOST, authority.as_str());
-        println!("YEAH");
-        RequestBuilder { address, builder }
+        Ok(RequestBuilder { address, builder })
     }
 }
 
@@ -60,8 +88,10 @@ pub struct RequestBuilder {
 }
 
 impl RequestBuilder {
-    fn get_mut_headers(&mut self) -> &mut HeaderMap {
-        self.builder.headers_mut().unwrap()
+    fn get_mut_headers(&mut self) -> PhpResult<&mut HeaderMap> {
+        self.builder
+            .headers_mut()
+            .ok_or_else(|| SpidroinError::new("Unable to get headers".to_string()).into())
     }
 }
 
@@ -76,53 +106,67 @@ impl RequestBuilder {
         #[this] this: &mut ZendClassObject<Self>,
         headers: HashMap<String, String>,
         update: Option<bool>,
-    ) -> &mut ZendClassObject<Self> {
-        let request_headers = this.get_mut_headers();
+    ) -> PhpResult<&mut ZendClassObject<Self>> {
+        let request_headers = this.get_mut_headers()?;
         let update = update.unwrap_or(false);
         for (key, value) in headers.iter() {
-            let key: HeaderName = key.try_into().unwrap();
-            let value = value.try_into().unwrap();
+            let key: HeaderName = key
+                .try_into()
+                .map_err(|err| SpidroinError::from("Unable to parse header name", &err))?;
+            let value = value
+                .try_into()
+                .map_err(|err| SpidroinError::from("Unable to parse header value", &err))?;
             if update {
                 request_headers.insert(key, value);
             } else {
                 request_headers.append(key, value);
             }
         }
-        this
+        Ok(this)
     }
 
     /// Send the request and return response.
     ///
     /// @return Response
-    pub fn send(&mut self) -> Response {
+    pub fn send(&mut self) -> PhpResult<Response> {
         let rt = get_runtime();
 
         let address = self.address.clone();
         let builder = std::mem::replace(&mut self.builder, Builder::new());
-        let req = builder.body(Empty::<Bytes>::new()).unwrap();
+        let req = builder
+            .body(Empty::<Bytes>::new())
+            .map_err(|err| SpidroinError::from("Unable to build body", &err))?;
 
         let res = rt.block_on(async move {
             // Open a TCP connection to the remote host
-            let stream = TcpStream::connect(address).await.unwrap();
+            let stream = TcpStream::connect(address)
+                .await
+                .map_err(|err| SpidroinError::from("Unable to establish connection", &err))?;
 
             // Perform a TCP handshake
-            let (mut sender, conn) = hyper::client::conn::http1::handshake(stream).await.unwrap();
+            let (mut sender, conn) = hyper::client::conn::http1::handshake(stream)
+                .await
+                .map_err(|err| SpidroinError::from("Unable to run handshake", &err))?;
 
             // spawn a task to poll the connection and drive the HTTP state
             tokio::task::spawn(async move {
-                conn.await.unwrap();
+                conn.await
+                    .map_err(|err| SpidroinError::from("Unable to poll connection", &err))
             });
 
             // Await the response...
-            sender.send_request(req).await.unwrap()
-        });
+            sender
+                .send_request(req)
+                .await
+                .map_err(|err| SpidroinError::from("Unable to send request", &err))
+        })?;
 
         let (parts, body) = res.into_parts();
 
-        Response {
+        Ok(Response {
             parts,
             body: Some(body),
-        }
+        })
     }
 }
 
