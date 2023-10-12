a48a5ec10b3f4f8ab2c217437f1ded70091c4359
vvvince <vvvince@ofcompute.rs>
2023-10-12 15:59:04 +0200

    chore(devenv): move initial setup to dedicated script

--- devenv.nix
+++ devenv.nix
@@ -29,19 +29,27 @@
    pkgs.openssl
   ];
 
+  scripts = {
+    setup.exec = ''
+      if [ ! -d php-src ]; then
+        just build-php
+      fi
+
+      if [ -z $(which cargo-php) ]; then
+        cargo install cargo-php
+      fi
+
+      if [ -z $(which cargo-audit) ]; then
+        cargo install cargo-audit
+      fi
+    '';
+  };
+
   enterShell = ''
     export CARGO_HOME="$PWD/.cargo"
     export PATH="$PWD/php-src/build/bin:$PWD/.cargo/bin/:$PWD/vendor/bin:$PATH"
 
     # Link the rust stdlib sources to a defined path to ease IDEs integration
     ln -sfT "$RUST_SRC_PATH" "$PWD/.rust-src"
-
-    if [ -z $(which cargo-php) ]; then
-      cargo install cargo-php
-    fi
-
-    if [ -z $(which cargo-audit) ]; then
-      cargo install cargo-audit
-    fi
   '';
 }
